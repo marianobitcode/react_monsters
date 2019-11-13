7fbf97ab04b2542c000bc4d7fc73dc382ed64c48
Mariano <marianobitcode@gmail.com>
2019-11-13 12:39:54 -0600

    Updates

---
+++ precache-manifest.b343645b6d4548db6fbba37fb8dcf4c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbe721e4f1dc05614c1ae40bcbb05bb",
+    "url": "/react_monsters/index.html"
+  },
+  {
+    "revision": "0a51d1b0d13a8801591e",
+    "url": "/react_monsters/static/css/main.220fe911.chunk.css"
+  },
+  {
+    "revision": "a20f2964f1e7103d046d",
+    "url": "/react_monsters/static/js/2.3860c490.chunk.js"
+  },
+  {
+    "revision": "0a51d1b0d13a8801591e",
+    "url": "/react_monsters/static/js/main.9890f674.chunk.js"
+  },
+  {
+    "revision": "d52d9a10593b2178dc93",
+    "url": "/react_monsters/static/js/runtime-main.4e38b442.js"
+  }
+]);
